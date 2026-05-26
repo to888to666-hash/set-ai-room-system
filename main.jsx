1f7a47e080fd7e2a7f3dafed0330317bb0dcd2b6
to888to666-hash <to888to666@gmail.com>
2026-05-26 16:02:31 +0800

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './style.css';
-import App from './App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
